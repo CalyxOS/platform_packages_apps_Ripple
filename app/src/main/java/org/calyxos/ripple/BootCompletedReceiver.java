e113b61838f5a1e6f54ca2be6fe29394b18dac28
Aayush Gupta <aayushgupta219@gmail.com>
2023-07-19 14:44:11 +0530

    Implement one shot migration notification
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
    Change-Id: Icb66afb34165957f054f488236dabedba292f613

---
+++ app/src/main/java/org/calyxos/ripple/BootCompletedReceiver.java
@@ -0,0 +1,84 @@
+package org.calyxos.ripple;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+import androidx.core.app.NotificationCompat;
+
+import java.util.Objects;
+
+import info.guardianproject.panic.PanicTrigger;
+
+public class BootCompletedReceiver extends BroadcastReceiver {
+
+    private static final String panicChannelID = "panic";
+    private static final int oneShotMigrationNotificationID = 100;
+    private static final String oneShotMigrationNotification = "oneShotMigrationNotification";
+    private static final String panicPreferences = "panicPreferences";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (context != null && Objects.equals(intent.getAction(), Intent.ACTION_BOOT_COMPLETED)) {
+            // Check and post migration notification if required
+            handleOneShotNotification(context);
+        }
+    }
+
+    private void handleOneShotNotification(Context ctx) {
+        SharedPreferences sharedPreferences = ctx
+                .getSharedPreferences(panicPreferences, Context.MODE_PRIVATE);
+
+        boolean shouldShowNotification =
+                !sharedPreferences.getBoolean(oneShotMigrationNotification, false)
+                        && PanicTrigger.getEnabledResponders(ctx).contains("org.fdroid.fdroid")
+                        && PanicTrigger.getAllResponders(ctx).contains("org.calyxos.panic");
+
+        if (shouldShowNotification) {
+            NotificationManager notificationManager =
+                    (NotificationManager) ctx.getSystemService(Context.NOTIFICATION_SERVICE);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                createNotificationChannel(ctx, notificationManager);
+            }
+            notificationManager.notify(oneShotMigrationNotificationID, getNotification(ctx));
+        }
+
+        sharedPreferences.edit().putBoolean(oneShotMigrationNotification, true).apply();
+    }
+
+    private Notification getNotification(Context context) {
+        Intent intent = new Intent(context, SettingsActivity.class);
+
+        PendingIntent contentIntent = PendingIntent
+                .getActivity(context, 0, intent, PendingIntent.FLAG_IMMUTABLE);
+
+        return new NotificationCompat.Builder(context, panicChannelID)
+                .setSmallIcon(R.drawable.ic_ripple_trigger)
+                .setContentTitle(context.getString(R.string.discover_panic_title))
+                .setContentText(context.getString(R.string.discover_panic_desc))
+                .setContentIntent(contentIntent)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setCategory(NotificationCompat.CATEGORY_SYSTEM)
+                .setAutoCancel(true)
+                .build();
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private void createNotificationChannel(Context ctx, NotificationManager notificationManager) {
+        NotificationChannel notificationChannel = new NotificationChannel(
+                panicChannelID,
+                ctx.getString(R.string.panic_channel_title),
+                NotificationManager.IMPORTANCE_DEFAULT
+        );
+        notificationChannel.setDescription(ctx.getString(R.string.panic_channel_desc));
+        notificationManager.createNotificationChannel(notificationChannel);
+    }
+}

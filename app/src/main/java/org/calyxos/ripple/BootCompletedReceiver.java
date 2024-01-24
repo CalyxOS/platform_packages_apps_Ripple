a892974b247ce7c5411245a63bd3a714941faa58
Tommy Webb <tommy@calyxinstitute.org>
2024-01-24 15:51:24 -0500

    Update info notification for Face Unlock
    Notify flame and coral users about Face Unlock availability in the next
    version. Copies and lightly modifies existing one-shot notification
    code.
    
    Change-Id: I180acaebe1dbfc09331e73355b0af05413bde329

--- app/src/main/java/org/calyxos/ripple/BootCompletedReceiver.java
+++ app/src/main/java/org/calyxos/ripple/BootCompletedReceiver.java
@@ -8,6 +8,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Build;
 
 import androidx.annotation.RequiresApi;
@@ -24,7 +25,11 @@ public class BootCompletedReceiver extends BroadcastReceiver {
     public static final String PANIC_SHARED_PREFS = "panicPreferences";
 
     private static final String panicChannelID = "panic";
+    private static final String updateInfoChannelID = "updateInfo";
     private static final int oneShotMigrationNotificationID = 100;
+    private static final int oneShotUpdateInfoNotificationID = 101;
+
+    private static final String UPDATE_INFO_URL = "https://calyxos.org";
 
     // Mirrored from PanicKit
     private static final String ENABLED_SHARED_PREFS =
@@ -61,6 +66,18 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         } else {
             panicPreferences.edit().putBoolean(ONE_SHOT_MIGRATION_NOTIFICATION_KEY, true).apply();
         }
+
+        boolean shouldShowUpdateInfoNotification =
+                Build.PRODUCT.equals("flame") || Build.PRODUCT.equals("coral");
+        if (shouldShowUpdateInfoNotification) {
+            NotificationManager notificationManager =
+                    (NotificationManager) ctx.getSystemService(Context.NOTIFICATION_SERVICE);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                createUpdateInfoNotificationChannel(ctx, notificationManager);
+            }
+            notificationManager.notify(oneShotUpdateInfoNotificationID,
+                    getUpdateInfoNotification(ctx));
+        }
     }
 
     private Notification getNotification(Context context) {
@@ -84,6 +101,29 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         return notification;
     }
 
+    private Notification getUpdateInfoNotification(Context context) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(UPDATE_INFO_URL));
+
+        PendingIntent contentIntent = PendingIntent
+                .getActivity(context, 0, intent, PendingIntent.FLAG_IMMUTABLE);
+
+        String longDescription = context.getString(R.string.update_info_desc_long);
+
+        Notification notification = new NotificationCompat.Builder(context, updateInfoChannelID)
+                .setSmallIcon(R.drawable.ic_notification_logo)
+                .setContentTitle(context.getString(R.string.update_info_title))
+                .setContentText(context.getString(R.string.update_info_desc))
+                .setStyle(new NotificationCompat.BigTextStyle().bigText(longDescription))
+                .setContentIntent(contentIntent)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setCategory(NotificationCompat.CATEGORY_SYSTEM)
+                .setAutoCancel(false)
+                .build();
+
+        notification.flags |= Notification.FLAG_NO_CLEAR;
+        return notification;
+    }
+
     @RequiresApi(Build.VERSION_CODES.O)
     private void createNotificationChannel(Context ctx, NotificationManager notificationManager) {
         NotificationChannel notificationChannel = new NotificationChannel(
@@ -92,6 +132,18 @@ public class BootCompletedReceiver extends BroadcastReceiver {
                 NotificationManager.IMPORTANCE_DEFAULT
         );
         notificationChannel.setDescription(ctx.getString(R.string.panic_channel_desc));
+        notificationManager.createNotificationChannel(notificationChannel);
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private void createUpdateInfoNotificationChannel(Context ctx,
+            NotificationManager notificationManager) {
+        NotificationChannel notificationChannel = new NotificationChannel(
+                updateInfoChannelID,
+                ctx.getString(R.string.update_info_channel_title),
+                NotificationManager.IMPORTANCE_DEFAULT
+        );
+        notificationChannel.setDescription(ctx.getString(R.string.update_info_channel_desc));
         notificationManager.createNotificationChannel(notificationChannel);
     }
 }
